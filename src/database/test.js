d756f322646c981bffb7a89dd230a4b3d6d93b02
vinicius-gaspar <vinicius-gaspar@live.com>
2020-08-07 15:41:19 -0300

    Initial Commit

---
+++ src/database/test.js
@@ -0,0 +1,36 @@
+const Database = require('./db')
+const createProffy = require('./createProffy')
+
+Database.then(async (db) => {
+    //inserir dados
+    proffyValue = {
+        name: "Diego Fernandes",
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        whatsapp: "43991373556",
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões."
+    }
+
+    classValue = {
+        subject: 1,
+        cost: "20"
+        //o proffy id vira pelo banco de dados
+    }
+
+    classScheduleValues = [
+        //class_id virá pelo banco de dados após cadastrarmos a class
+        {
+            weekday: 1,
+            time_from: 720,
+            time_to: 1220 
+        },
+        {
+            weekday: 0,
+            time_from: 520,
+            time_to: 1220 
+        }
+    ]
+
+    await createProffy(db, {proffyValue, classValue, classScheduleValues})
+
+    //consultar os dados inseridos
+})
